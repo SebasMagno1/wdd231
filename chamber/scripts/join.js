0a85278e75086b230725da0efc4ec65ba3e0e1d1
SebasMagno1 <114171307+SebasMagno1@users.noreply.github.com>
2026-06-04 07:30:02 -0600

    Add files via upload

---
+++ chamber/scripts/join.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const timestampField = document.getElementById("timestamp");
+
+    timestampField.value = new Date().toISOString();
+
+});
